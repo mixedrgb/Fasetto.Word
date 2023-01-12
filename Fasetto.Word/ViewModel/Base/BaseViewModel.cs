9a697aff0164cbb7b2114b4ff7d56c2bf9ff106d
mixedrgb <88474004+mixedrgb@users.noreply.github.com>
2023-01-11 21:15:57 -0600

    Move base files into base folder

---
+++ Fasetto.Word/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Base view model that "fires" Property Changed events as needed
+    /// </summary>
+    //[AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : AddINotifyPropertyChangedInterfaceAttribute
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
