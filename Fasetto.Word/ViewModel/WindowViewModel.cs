e9f1af16f685b0254beec290bba6ab71baee36b2
mixedrgb <88474004+mixedrgb@users.noreply.github.com>
2023-01-11 21:16:07 -0600

    all the other frick

---
+++ Fasetto.Word/ViewModel/WindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// The View Model for the custom Window Styling
+    /// </summary>
+    public class WindowViewModel : BaseViewModel
+    {
+        #region Private Members
+
+        /// <summary>
+        /// The window this view model controls
+        /// </summary>
+        private Window _mWindow;
+
+        #endregion
+
+        #region Public Properties
+        
+        public string Test { get; set; } = "My string";
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public WindowViewModel(Window window)
+        {
+
+        }
+        #endregion
+    }
+}
