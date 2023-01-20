2b5d186017f51c5b7a49a10d4f9c2723aa899cf3
mixedrgb <88474004+mixedrgb@users.noreply.github.com>
2023-01-19 20:36:05 -0600

    Add value converters

---
+++ Fasetto.Word/ValueConverters/ApplicationPageValueConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word
+{
+    public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        /// <summary>
+        /// Converters the <see cref="ApplicationPage">to an actual view/page</see>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.Login:
+                    return new LoginPage();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
