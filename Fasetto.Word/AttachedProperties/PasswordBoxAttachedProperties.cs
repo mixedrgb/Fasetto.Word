c568277977caac92516e8946aa3eff109fd0dd47
mixedrgb <88474004+mixedrgb@users.noreply.github.com>
2023-01-22 17:13:44 -0600

    I don't know what's happening lmao

--- Fasetto.Word/AttachedProperties/PasswordBoxAttachedProperties.cs
+++ Fasetto.Word/AttachedProperties/PasswordBoxAttachedProperties.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace Fasetto.Word
 {
+
     /// <summary>
     /// The MonitorPassword attached property for a <see cref="PasswordBox"/>
     /// </summary>
