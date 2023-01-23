7004c4f8ea8d14007e89d523bc6f820b2098a339
mixedrgb <88474004+mixedrgb@users.noreply.github.com>
2023-01-22 20:51:35 -0600

    Add a temporary button lick to show animation

--- Fasetto.Word/Pages/LoginPage.xaml.cs
+++ Fasetto.Word/Pages/LoginPage.xaml.cs
@@ -1,15 +1,22 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Fasetto.Word
 {
     /// <summary>
     /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class LoginPage : Page
+    public partial class LoginPage : BasePage
     {
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.AnimateOut();
         }
     }
 }
