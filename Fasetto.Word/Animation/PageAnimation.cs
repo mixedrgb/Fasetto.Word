ddd65146001b68396d4056965b9be945b54d55c4
mixedrgb <88474004+mixedrgb@users.noreply.github.com>
2023-01-24 20:12:57 -0600

    Minor formatting

--- Fasetto.Word/Animation/PageAnimation.cs
+++ Fasetto.Word/Animation/PageAnimation.cs
@@ -9,10 +9,12 @@
         /// No animation takes place
         /// </summary>
         None = 0,
+
         /// <summary>
         /// The page slides in and fades in from the right
         /// </summary>
         SlideAndFadeInFromRight,
+
         /// <summary>
         /// The page slides in and fades in from the left
         /// </summary>
